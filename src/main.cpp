c15bb4a82b637853efe5d54c632333c22935cfa4
Johan van der Vyver <johanvdvyver@Johans-MacBook-Pro.local>
2021-01-01 17:52:50 +0200

    Add some comments to code to clarify things

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,38 @@
 #include "soc/rtc_wdt.h"
 #include "esp_int_wdt.h"
 #include "esp_task_wdt.h"
-#include "Arduino.h"
 #include "main.h"
 #include "multicore_main.h"
+#include "Arduino.h"
+
+// Do not modify anything here, instead make use of multicore_main.cpp
 
 // This sets up the ESP32 to run two independent setup() and loop() functions
 // per CPU: core0_setup(), core1_setup(), core0_loop(), core1_loop()
 // Those can be modified in multicore_main.cpp
-//
-// This file focuses on the setup to make those functions act as independent
-// arduino apps with shared memory and a locking mechanism:
-// synchronized(
-//   printf("I can only run on one core at a time");
-//   delay(5);
-// )
 
 #define TASK_NAME(core_number) "core ## core_number ## task"
 #define TASK_HANDLE(core_number) core ## core_number ## _task
-#define CREATE_TASK(core_number) \
-  xTaskCreatePinnedToCore([](void* _) TASK_CODE(core_number), TASK_NAME(core_number), STACK_BYTE_SIZE, NULL, 100, &TASK_HANDLE(core_number), core_number);
-#define TASK_CODE(core_number) { \
-  core ## core_number ## _setup(); \
-  while(1) { \
-    core ## core_number ## _loop(); \
-  } \
-}
 
-static SemaphoreHandle_t synchronize_semaphore;
+#define CREATE_TASK(core_number) \
+  xTaskCreatePinnedToCore([](void* _) TASK_CODE(core_number), TASK_NAME(core_number), STACK_BYTE_SIZE, NULL, 100, &TASK_HANDLE(core_number), core_number); \
+  delay(TASK_CREATE_DELAY_MS)
+
+#define TASK_CODE(core_number) { \
+    core ## core_number ## _setup(); \
+    while(1) { \
+      core ## core_number ## _loop(); \
+    } \
+  }
+
+static SemaphoreHandle_t synchronized_semaphore;
 static TaskHandle_t core0_task;
 static TaskHandle_t core1_task;
 
 void setup() {
+  // Semaphore for synchronized block
+  synchronized_semaphore = xSemaphoreCreateMutex();
+
   // Disable all the watchdogs.  The watchdogs kill performance
   // and resets the ESP32 at the drop of a hat
   rtc_wdt_protect_off();
@@ -42,28 +43,25 @@ void setup() {
   disableCore1WDT();
   disableLoopWDT();
 
-  // Semaphore for synchronized block
-  synchronize_semaphore = xSemaphoreCreateMutex();
-
   // Create the setup and loop for core 0
   CREATE_TASK(0);
-  delay(1000);
 
   // Create the setup and loop for core 1
   CREATE_TASK(1);
-  delay(1000);
 
   // Completely delete the loop/main thread
   vTaskDelete(NULL);
 }
 
-void _synchronize_semaphore_lock() {
-  xSemaphoreTake(synchronize_semaphore, portMAX_DELAY);
+void synchronized_semaphore_lock() {
+  xSemaphoreTake(synchronized_semaphore, portMAX_DELAY);
 }
 
-void _synchronize_semaphore_unlock() {
-  xSemaphoreGive(synchronize_semaphore);
+void synchronized_semaphore_unlock() {
+  xSemaphoreGive(synchronized_semaphore);
 }
 
+// This will never get executed, this function is just here to allow linking to succeed
+// Putting anything here is pointless
 void loop() {
 }
--- src/main.h
+++ src/main.h
@@ -4,21 +4,25 @@
 extern "C" {
 #endif
 
-  // The size of the each thread stack specified as the number of bytes
-  #define STACK_BYTE_SIZE  (1024 * 8)
+  #define STACK_BYTE_SIZE       (1024 * 8)  // The size of the each thread stack specified as the number of bytes
+  #define TASK_CREATE_DELAY_MS  500         // After creating a task how many milliseconds to delay before doing something else
+
+  // Code that should execute on a single core alone:
+  // synchronized(
+  //   some_variable++;  // Increment volatile integer
+  //   some_ref = NULL;  // Update a volatile pointer
+  // );
+  #define synchronized(__volatile_code_synchronized_across_cores) \
+    do { \
+      synchronized_semaphore_lock(); \
+      __volatile_code_synchronized_across_cores \
+      synchronized_semaphore_unlock(); \
+    } while(0)
 
   // Do not use these functions directly to avoid the possiblity of forgetting to unlock
-  void _synchronize_semaphore_lock();
-  void _synchronize_semaphore_unlock();
-
-  #define synchronized(__code_to_be_synchronized_across_cores__) \
-    do { \
-      _synchronize_semaphore_lock(); \
-      do { \
-        __code_to_be_synchronized_across_cores__ \
-      } while(0); \
-      _synchronize_semaphore_unlock(); \
-    } while(0)
+  // Instead use synchronized above
+  void synchronized_semaphore_lock();
+  void synchronized_semaphore_unlock();
 
 #ifdef __cplusplus
 }
